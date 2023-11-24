3e6583546004d5f2f6bef746c8fbf19eac741213
Zach Willis <zachmw27@gmail.com>
2023-11-23 20:07:02 -0500

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    })
+  });
